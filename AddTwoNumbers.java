d2e303d4de78a8039fe8f377e6323b5a467ecaab
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-02-27 13:24:14 +0530

    Add two numbers

---
+++ AddTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class AddTwoNumbers{
+public static void main(String[] args){
+
+Scanner sc=new Scanner(System.in);
+int input1=sc.nextInt();
+int input2=sc.nextInt();
+
+System.out.print("Sum: "+ (input1+input2));
+
+
+
+}
+}
